5689d2f1fdd4903fb48b5815c843eb3e36e1893d
Ballofmoss <ivansvenn@ya.ru>
2025-04-02 20:12:07 +0400

    Filters

--- app/products/views.py
+++ app/products/views.py
@@ -1,5 +1,5 @@
-from gc import get_objects
 from django.core.paginator import Paginator
+from django.http import Http404
 from django.shortcuts import get_list_or_404, render
 from products.models import Products
 
@@ -7,13 +7,21 @@ from products.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
 
     if category_slug == "all":
         products = Products.objects.all()
     else:
-        products = get_list_or_404(
-            Products.objects.filter(category__slug=category_slug)
-        )
+        products = Products.objects.filter(category__slug=category_slug)
+        if not products.exists():
+            raise Http404()("No products found for this category.")
+
+    if on_sale:
+        products = products.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        products = products.order_by(order_by)
 
     paginator = Paginator(products, 3)
 
