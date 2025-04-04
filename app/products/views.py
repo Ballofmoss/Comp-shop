d4e0388a84b5aacd2dd375e2107cd53d225aa9ce
Ballofmoss <ivansvenn@ya.ru>
2025-04-04 14:09:20 +0400

    fixtures and search

--- app/products/views.py
+++ app/products/views.py
@@ -1,21 +1,25 @@
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_list_or_404, render
+from products.utils import q_search
 from products.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get("page", 1)
     on_sale = request.GET.get("on_sale", None)
     order_by = request.GET.get("order_by", None)
+    query = request.GET.get("q", None)
 
     if category_slug == "all":
         products = Products.objects.all()
+    elif query:
+        products = q_search(query)
     else:
         products = Products.objects.filter(category__slug=category_slug)
         if not products.exists():
-            raise Http404()("No products found for this category.")
+            raise Http404("No products found for this category.")
 
     if on_sale:
         products = products.filter(discount__gt=0)
