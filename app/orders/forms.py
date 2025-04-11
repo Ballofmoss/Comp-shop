3e213614a8009d29f849df2bf34b3a70e8f8a690
Ballofmoss <ivansvenn@ya.ru>
2025-04-11 22:43:07 +0400

    Final edits

--- app/orders/forms.py
+++ app/orders/forms.py
@@ -11,24 +11,24 @@ class CreateOrderForm(forms.Form):
         choices=[
             ("0", False),
             ("1", True),
-            ],
-        )
+        ],
+    )
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(
         choices=[
-            ("0", 'False'),
-            ("1", 'True'),
-            ],
-        )
+            ("0", "False"),
+            ("1", "True"),
+        ],
+    )
 
-    def clean_phone_number(self):
-        data = self.cleaned_data['phone_number']
+    # def clean_phone_number(self):
+    #     data = self.cleaned_data["phone_number"]
 
-        if not data.isdigit():
-            raise forms.ValidationError("Номер телефона должен содержать только цифры")
-        
-        pattern = re.compile(r'^\d{10}$')
-        if not pattern.match(data):
-            raise forms.ValidationError("Неверный формат номера")
+    #     if not data.isdigit():
+    #         raise forms.ValidationError("Номер телефона должен содержать только цифры")
 
-        return data
+    #     pattern = re.compile(r"^\d{10}$")
+    #     if not pattern.match(data):
+    #         raise forms.ValidationError("Неверный формат номера")
+
+    #     return data
--- app/users/admin.py
+++ app/users/admin.py
@@ -1,6 +1,7 @@
 from re import search
 from django.contrib import admin
 
+from orders.admin import OrderTabulareAdmin
 from carts.admin import CartTabAdmin
 from users.models import User
 
@@ -12,6 +13,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email"]
     search_field = ["username", "first_name", "last_name", "email"]
 
-    inlines = [
-        CartTabAdmin,
-    ]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
